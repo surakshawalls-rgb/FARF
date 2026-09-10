d36092b90f49705eccbca77d6812c9cd271524e7
Supermar1718 <supermarket201917@gmail.com>
2026-09-10 16:55:14 +0530

    enhanced UI and look

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -3,10 +3,9 @@ import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
+  standalone: true,
   imports: [RouterModule],
   templateUrl: './home.html',
-  styleUrl: './home.scss',
+  styleUrl: './home.scss'
 })
-export class Home {
-
-}
+export class Home {}
--- src/app/pages/team/team.ts
+++ src/app/pages/team/team.ts
@@ -2,10 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-team',
+  standalone: true,
   imports: [],
   templateUrl: './team.html',
-  styleUrl: './team.scss',
+  styleUrl: './team.scss'
 })
-export class Team {
-
-}
+export class Team {}
