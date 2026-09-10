c061e53a4a51106318c57a704eb4c1c9f9b83fd3
Pradeep <supermarket201917@gmail.com>
2026-09-10 14:39:24 +0530

    updated Prince Agrawal Profile

---
+++ android/app/src/main/java/com/example/farf/MainActivity.java
@@ -0,0 +1,5 @@
+package com.example.farf;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.farf',
+  appName: 'FARF',
+  webDir: 'dist/farf/browser'
+};
+
+export default config;
